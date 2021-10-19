14c4abf44c1e19c4c49abd8cd00006dd7fcba596
wang_chao <chao.wang@guanaitong.com>
2021-10-19 18:14:34 +0800

    init

---
+++ src/main/java/com/github/supermoonie/iphone/monitor/App.java
@@ -0,0 +1,12 @@
+package com.github.supermoonie.iphone.monitor;
+
+/**
+ * @author supermoonie
+ * @since 2021/10/19
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/github/supermoonie/iphone/monitor/AppTest.java
@@ -0,0 +1,11 @@
+package com.github.supermoonie.iphone.monitor;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author supermoonie
+ * @since 2021/10/19
+ */
+public class AppTest {
+
+}
